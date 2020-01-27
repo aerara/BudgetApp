89b017d062e4bbe7623da9fca8cd0a82dc357a0e
black_raBB1t <renzy.verano@student.passerellesnumeriques.org>
2020-01-27 10:33:54 +0800

    Initial commit

---
+++ BudgetApp/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsComponent } from './products/products.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    DashboardComponent,
+    CartComponent,
+    ProductsComponent,
+    AdminLoginComponent,
+    AdminDashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
