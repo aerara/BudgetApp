cda249eeda0b88bf0c231e60881f3907dd920c0b
black_raBB1t <renzy.verano@student.passerellesnumeriques.org>
2020-01-28 08:53:32 +0800

    Add The Admin Component with Swal Module

--- BudgetApp/src/app/admin-login/admin-login.component.ts
+++ BudgetApp/src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+// import Swal from 'sweetalert2';
+// import swal from 'swal';
 
 @Component({
   selector: 'app-admin-login',
@@ -6,12 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent implements OnInit {
+  public userName="Admin"
+  public userPass="@dm1n"
+  public userInput : string;
+  public passInput : string;
 
 
-
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
 
+  onSubmit(){
+    if(this.passInput === this.userPass && this.userInput === this.userName){
+      console.log("Nakasulod Naka")
+      this.router.navigate(["/admin/dashboard"])
+    }
+  }
 }
